6f9bd2fa51532b864d5856f02e0d00092dfe502a
Natalie Albrecht <nataliealb@ebay.com>
2018-10-07 10:47:31 -0700

    Fix issue with loading date only calendar events

--- app/src/main/java/org/oregongoestocollege/itsaplan/data/MyPlanRepository.java
+++ app/src/main/java/org/oregongoestocollege/itsaplan/data/MyPlanRepository.java
@@ -11,6 +11,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.text.TextUtils;
 
+import org.oregongoestocollege.itsaplan.SingleLiveEvent;
 import org.oregongoestocollege.itsaplan.Utils;
 import org.oregongoestocollege.itsaplan.data.dao.BlockInfoDao;
 import org.oregongoestocollege.itsaplan.data.dao.CollegeDao;
@@ -59,7 +60,7 @@ public class MyPlanRepository
 		allBlockInfos = blockInfoDao.getAll();
 		visitedKeyDao = database.visitedKeyDao();
 
-		allCalendarEvents = new MutableLiveData<>();
+		allCalendarEvents = new SingleLiveEvent<>();
 	}
 
 	public static MyPlanRepository getInstance(@NonNull Context context)
